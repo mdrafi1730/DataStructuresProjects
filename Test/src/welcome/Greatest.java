377e79d340945edb5119d874113604f9fc166bb3
mdraf <mdraf@192.168.1.17>
2023-04-12 23:18:25 +0300

    Greatest Two Nos Commit @11:18 PM

---
+++ Test/src/welcome/Greatest.java
@@ -0,0 +1,15 @@
+package welcome;
+
+public class Greatest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 30;
+		int b = 20;
+		if(a > b)
+			System.out.println(a + " Greater than " + b);
+		else
+			System.out.println(b + " Greater than " + a);
+	}
+
+}
