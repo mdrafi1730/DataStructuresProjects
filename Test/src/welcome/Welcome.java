4461d3161d846678bab4cb6e8a6a0a5b84cf16b5
mdraf <mdraf@192.168.1.17>
2023-04-12 19:43:26 +0300

    First Commit @07:43

---
+++ Test/src/welcome/Welcome.java
@@ -0,0 +1,14 @@
+package welcome;
+
+import javax.swing.JOptionPane;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Welcome to JAVA programming");
+		System.out.println("Welcome to JAVA programming");
+
+	}
+
+}
