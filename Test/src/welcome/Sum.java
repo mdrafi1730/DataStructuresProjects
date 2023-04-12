9ee5db90b8696fd5896ab4d5cdae2e1037402fc3
mdraf <mdraf@192.168.1.17>
2023-04-12 23:04:31 +0300

    Commit @11:04 PM

--- Test/src/welcome/Sum.java
+++ Test/src/welcome/Sum.java
@@ -2,6 +2,6 @@ package welcome;
 public class Sum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("Sum = " + (10 + 20));
+		System.out.println("Sum = " + (10 + 20));
   }
 }
